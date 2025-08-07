cc2c0b6cdc5f266794ead2bdce7b0d9f42118c6d
sanyuan.hy <sanyuan.hy@alibaba-inc.com>
2025-08-07 17:17:45 +0800

    feature: debug complete_embedding_matrix

--- algorithms/utils.py
+++ algorithms/utils.py
@@ -513,8 +513,10 @@ def load_trained_model(model_config, args, model_class, model_dirpath):
                 name = k
             if name in model_state_dict_keys:
                 new_state_dict[name] = v
-        print("diff:")
-        print(model_state_dict_keys.difference(new_state_dict.keys()))
+        diff = model_state_dict_keys.difference(new_state_dict.keys())
+        if len(diff) > 0:
+            print("diff:")
+            print(diff)
         model.load_state_dict(new_state_dict)
     return model
 
