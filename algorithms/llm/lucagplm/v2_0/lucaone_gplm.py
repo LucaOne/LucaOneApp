a2e1ea76678d29c6d70c5e56eca977c138bdbb25
sanyuan.hy <sanyuan.hy@alibaba-inc.com>
2025-08-11 16:24:26 +0800

    feature: debug complete_embedding_matrix

--- algorithms/llm/lucagplm/v2_0/lucaone_gplm.py
+++ algorithms/llm/lucagplm/v2_0/lucaone_gplm.py
@@ -16,10 +16,10 @@ sys.path.append("..")
 sys.path.append("../../")
 sys.path.append("../../algorithms")
 try:
-    from ..common.loss import *
-    from .model_utils import AllOutput, create_output_loss_lucagplm
-    from .alphabet import Alphabet
-    from .modeling_gplm import *
+    from common.loss import *
+    from llm.lucagplm.v2_0.model_utils import AllOutput, create_output_loss_lucagplm
+    from llm.lucagplm.v2_0.alphabet import Alphabet
+    from llm.lucagplm.v2_0.modeling_gplm import *
 except ImportError:
     from algorithms.common.loss import *
     from algorithms.llm.lucagplm.v2_0.model_utils import AllOutput, create_output_loss_lucagplm
@@ -58,9 +58,12 @@ class LucaGPLM(nn.Module):
         self.use_embed_layer_norm = config.use_embed_layer_norm
         self.use_last_layer_norm = config.use_last_layer_norm
         self.embed_scale = config.embed_scale
-        self.pretrained_model_name = args.pretrained_model_name
+        if args and hasattr(args, "pretrained_model_name"):
+            self.pretrained_model_name = args.pretrained_model_name
+        else:
+            self.pretrained_model_name = None
         # 如果只用于embedding 推理则有些层不加载与构建
-        if hasattr(args, "embedding_inference"):
+        if args and hasattr(args, "embedding_inference"):
             self.embedding_inference = args.embedding_inference
         else:
             self.embedding_inference = False
@@ -411,21 +414,22 @@ class LucaGPLM(nn.Module):
         # 最后一层作为表征矩阵
         # (B, L, E)
         representation_matrix = hidden_representations[self.layer_size]
-        # mask 任务
-        # B * Seq_len * vocab_size
-        if not self.embedding_inference:
-            lm_mask_logits = self.lm_head(x)
-        # lm head的输出向量作为表征向量
         # (B, E)
         representation_vector = representation_matrix[:, 0, :]
-
-        logits = {}
-        losses = {}
-        outputs = {}
         representations = {
             "representation_matrix": representation_matrix,
             "representation_vector": representation_vector
         }
+        # mask 任务
+        # B * Seq_len * vocab_size
+        if not self.embedding_inference:
+            lm_mask_logits = self.lm_head(x)
+
+
+        logits = {}
+        losses = {}
+        outputs = {}
+
         # 每一层的attention值
         if need_head_weights:
             # attentions: B x Layers x H x L x L
@@ -440,10 +444,6 @@ class LucaGPLM(nn.Module):
                     and not self.embedding_inference:
                 contacts = self.contact_head(input_ids, attentions)
                 representations["contacts"] = contacts
-        '''
-        print("output_keys:")
-        print(output_keys)
-        '''
         if not self.embedding_inference and output_keys:
             for item in output_keys.items():
                 cur_task_level_type = item[0]
